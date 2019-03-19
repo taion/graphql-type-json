c729016f1d4a6ebecbcd92890b1c9462d98787d5
Jimmy Jia <tesrin@gmail.com>
2019-03-19 13:12:20 -0400

    chore: Update dependencies

--- src/index.js
+++ src/index.js
@@ -37,9 +37,7 @@ function parseLiteral(ast, variables) {
 export default new GraphQLScalarType({
   name: 'JSON',
   description:
-    'The `JSON` scalar type represents JSON values as specified by ' +
-    '[ECMA-404](http://www.ecma-international.org/' +
-    'publications/files/ECMA-ST/ECMA-404.pdf).',
+    'The `JSON` scalar type represents JSON values as specified by [ECMA-404](http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf).',
   serialize: identity,
   parseValue: identity,
   parseLiteral,
