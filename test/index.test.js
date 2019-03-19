bbe532868b145176d1b81341962b3a9c94e233a4
Jimmy Jia <tesrin@gmail.com>
2019-03-19 13:19:33 -0400

    chore: Update dependencies (#50)

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,9 @@
-import { graphql, GraphQLInt, GraphQLObjectType, GraphQLSchema } from 'graphql';
+import {
+  graphql,
+  GraphQLInt,
+  GraphQLObjectType,
+  GraphQLSchema,
+} from 'graphql';
 
 import GraphQLJSON from '../src';
 
@@ -17,14 +22,7 @@ const FIXTURE = {
     false: true,
     null: null,
   },
-  array: [
-    'string',
-    3,
-    Math.PI,
-    true,
-    false,
-    null,
-  ],
+  array: ['string', 3, Math.PI, true, false, null],
 };
 
 describe('GraphQLJSON', () => {
@@ -58,48 +56,41 @@ describe('GraphQLJSON', () => {
 
   describe('parseValue', () => {
     it('should support parsing values', () =>
-      graphql(
-        schema,
-        'query ($arg: JSON!) { value(arg: $arg) }',
-        null,
-        null,
-        { arg: FIXTURE },
-      ).then(({ data }) => {
+      graphql(schema, 'query ($arg: JSON!) { value(arg: $arg) }', null, null, {
+        arg: FIXTURE,
+      }).then(({ data }) => {
         expect(data.value).toEqual(FIXTURE);
-      }),
-    );
+      }));
   });
 
   describe('parseLiteral', () => {
     it('should support parsing literals', () =>
-      graphql(schema, `
-        query ($intValue: Int = 3) {
-          value(arg: {
-            string: "string",
-            int: $intValue,
-            float: 3.14,
-            true: true,
-            false: false,
-            null: null,
-            object: {
-              string: "string",
-              int: $intValue,
-              float: 3.14,
-              true: true,
-              false: false,
-              null: null,
-            },
-            array: [
-              "string",
-              $intValue,
-              3.14,
-              true,
-              false,
-              null,
-            ],
-          }),
-        }
-      `).then(({ data }) => {
+      graphql(
+        schema,
+        `
+          query($intValue: Int = 3) {
+            value(
+              arg: {
+                string: "string"
+                int: $intValue
+                float: 3.14
+                true: true
+                false: false
+                null: null
+                object: {
+                  string: "string"
+                  int: $intValue
+                  float: 3.14
+                  true: true
+                  false: false
+                  null: null
+                }
+                array: ["string", $intValue, 3.14, true, false, null]
+              }
+            )
+          }
+        `,
+      ).then(({ data }) => {
         expect(data.value).toEqual({
           string: 'string',
           int: 3,
@@ -115,38 +106,34 @@ describe('GraphQLJSON', () => {
             false: false,
             null: null,
           },
-          array: [
-            'string',
-            3,
-            3.14,
-            true,
-            false,
-            null,
-          ],
+          array: ['string', 3, 3.14, true, false, null],
         });
-      }),
-    );
+      }));
 
     it('should handle null literals', () =>
-      graphql(schema, `
-        {
-          value(arg: null)
-        }
-      `).then(({ data }) => {
+      graphql(
+        schema,
+        `
+          {
+            value(arg: null)
+          }
+        `,
+      ).then(({ data }) => {
         expect(data).toEqual({
           value: null,
         });
-      }),
-    );
+      }));
 
     it('should reject invalid literals', () =>
-      graphql(schema, `
-        {
-          value(arg: INVALID)
-        }
-      `).then(({ data }) => {
+      graphql(
+        schema,
+        `
+          {
+            value(arg: INVALID)
+          }
+        `,
+      ).then(({ data }) => {
         expect(data).toBeUndefined();
-      }),
-    );
+      }));
   });
 });
