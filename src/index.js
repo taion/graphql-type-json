396387382c82c1f34e39a5727fa463668850d831
Jimmy Jia <tesrin@gmail.com>
2019-03-29 18:52:11 -0400

    feat: Add GraphQLJSONObject
    BREAKING CHANGE: Change CommonJS exports

--- src/index.js
+++ src/index.js
@@ -5,6 +5,26 @@ function identity(value) {
   return value;
 }
 
+function ensureObject(value) {
+  if (typeof value !== 'object' || value === null || Array.isArray(value)) {
+    throw new TypeError(
+      `JSONObject cannot represent non-object value: ${value}`,
+    );
+  }
+
+  return value;
+}
+
+function parseObject(ast, variables) {
+  const value = Object.create(null);
+  ast.fields.forEach(field => {
+    // eslint-disable-next-line no-use-before-define
+    value[field.name.value] = parseLiteral(field.value, variables);
+  });
+
+  return value;
+}
+
 function parseLiteral(ast, variables) {
   switch (ast.kind) {
     case Kind.STRING:
@@ -13,14 +33,8 @@ function parseLiteral(ast, variables) {
     case Kind.INT:
     case Kind.FLOAT:
       return parseFloat(ast.value);
-    case Kind.OBJECT: {
-      const value = Object.create(null);
-      ast.fields.forEach(field => {
-        value[field.name.value] = parseLiteral(field.value, variables);
-      });
-
-      return value;
-    }
+    case Kind.OBJECT:
+      return parseObject(ast, variables);
     case Kind.LIST:
       return ast.values.map(n => parseLiteral(n, variables));
     case Kind.NULL:
@@ -34,11 +48,24 @@ function parseLiteral(ast, variables) {
   }
 }
 
-export default new GraphQLScalarType({
+// This named export is intended for users of CommonJS. Users of ES modules
+// should instead use the default export.
+export const GraphQLJSON = new GraphQLScalarType({
   name: 'JSON',
   description:
     'The `JSON` scalar type represents JSON values as specified by [ECMA-404](http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf).',
   serialize: identity,
   parseValue: identity,
   parseLiteral,
+});
+
+export default GraphQLJSON;
+
+export const GraphQLJSONObject = new GraphQLScalarType({
+  name: 'JSONObject',
+  description:
+    'The `JSONObject` scalar type represents JSON objects as specified by [ECMA-404](http://www.ecma-international.org/publications/files/ECMA-ST/ECMA-404.pdf).',
+  serialize: ensureObject,
+  parseValue: ensureObject,
+  parseLiteral: parseObject,
 });
